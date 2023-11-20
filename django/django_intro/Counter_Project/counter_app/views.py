6bf793edf1d7d792da17f7e1a1bc370b5b5d305e
Ayah Safi <ayaadnansafi@gmail.com>
2023-11-20 14:52:04 +0200

    Counter Assignment

---
+++ django/django_intro/Counter_Project/counter_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+
+def counter(request):
+    if 'count' not in request.session:
+        request.session['count'] = 1
+    else:
+        request.session['count'] += 1
+    return render(request,'index.html')
+
+def destroy(request):
+    del request.session['count']
+    return redirect('/')	
